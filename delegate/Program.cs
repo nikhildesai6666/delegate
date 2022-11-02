b39e9524c5caf1119e1a989a7cb39d4af53133b0
Nikhil <Nikhil@DESKTOP-NEFOSTI>
2022-11-02 12:45:30 +1030

    Add project files.

---
+++ delegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @delegate
+{
+  
+    public delegate int MyDelegate(int a, int b);
+    public class Calc
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+            MyDelegate myDelegate = new MyDelegate(calc.Add); 
+            int sum = myDelegate.Invoke(45, 23);
+            Console.WriteLine(sum);
+        }
+
+
+    }
+
+
+}
